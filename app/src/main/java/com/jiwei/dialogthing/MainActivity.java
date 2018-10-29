ef833c9931b88a01c1a663b784296aca386972eb
unknown <18099@CORP.NARI-RELAYS.COM>
2018-10-29 15:58:30 +0800

    create library

--- app/src/main/java/com/jiwei/dialogthing/MainActivity.java
+++ app/src/main/java/com/jiwei/dialogthing/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jiwei.dialogutil.DialogUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
         DialogUtil dialogUtil = new DialogUtil(this);
         Dialog dialog = dialogUtil.getLoadingDialog("加载中",true);
         dialog.show();
-
-
 
     }
 }
--- dialogutil/src/main/java/com/jiwei/dialogutil/DialogUtil.java
+++ dialogutil/src/main/java/com/jiwei/dialogutil/DialogUtil.java
@@ -1,4 +1,4 @@
-package com.jiwei.dialogthing;
+package com.jiwei.dialogutil;
 
 import android.app.Dialog;
 import android.content.Context;
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
